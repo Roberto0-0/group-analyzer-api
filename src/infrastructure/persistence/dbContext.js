3acfa4714befc34670db8ac29cdb44242638a2fa
Robert <roberto88526@gmail.com>
2025-08-28 17:17:28 -0300

    feat(drizzle): Connecting to SQLite.

---
+++ src/infrastructure/persistence/dbContext.js
@@ -0,0 +1,10 @@
+import { drizzle } from "drizzle-orm/better-sqlite3";
+import Database from "better-sqlite3";
+import { join } from "node:path";
+(await import("dotenv")).config({
+    path: join(process.cwd(), ".env"),
+    quiet: true
+});
+
+const sqlite = new Database(join(process.cwd(), process.env.DATA_SOURCE_DEV));
+export const db = drizzle(sqlite);
