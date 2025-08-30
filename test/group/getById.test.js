947bb5c28efee6abf5b5c094a8bdb022e8826678
Robert <roberto88526@gmail.com>
2025-08-29 23:39:53 -0300

    test(group): New tests with the SQLite repository.

--- test/group/getById.test.js
+++ test/group/getById.test.js
@@ -1,14 +1,18 @@
 import test from "node:test";
 import assert from "node:assert/strict";
-import { GroupInMemoryRespository } from "../../src/infrastructure/repositories/groupInMemoryRespository.js";
+import { GroupSQLiteRespository } from "../../src/infrastructure/repositories/groupSQLiteRepository.js";
 import { GroupGetById } from "../../src/application/usecases/group/groupGetByIdUsecase.js";
 
-test("should get group by id.", () => {
-    const id = "293902030";
+test("should get group by id.", async () => {
+    const id = "2039403040";
 
-    const groupRepository = new GroupInMemoryRespository();
+    const groupRepository = new GroupSQLiteRespository();
     const getGroupByIdUsecase = new GroupGetById(groupRepository);
-    const response = getGroupByIdUsecase.execute(id);
+    const response = await getGroupByIdUsecase.execute(id);
 
-    assert.equal(response, null);
+    if(!response.success) assert.fail(response.message);
+
+    console.log(response);
+
+    assert.equal(response.data.id, id);
 });
