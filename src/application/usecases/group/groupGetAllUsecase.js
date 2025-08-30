e514cdd637778d141861afba33331c4fc6e53a30
Robert <roberto88526@gmail.com>
2025-08-29 23:32:53 -0300

    feat(group): Implemented SQLite repository.
    Support for the new SQLite repository with Drizzle ORM

--- src/application/usecases/group/groupGetAllUsecase.js
+++ src/application/usecases/group/groupGetAllUsecase.js
@@ -1,20 +1,23 @@
-import { Group } from "../../../domain/entities/Group.js";
-import { GroupInMemoryRespository } from "../../../infrastructure/repositories/groupInMemoryRespository.js";
+import { GroupSQLiteRespository } from "../../../infrastructure/repositories/groupSQLiteRepository.js";
+import { Result } from "../../common/result.js";
 
 export class GroupGetAllUsecase {
-    /** @property {GroupInMemoryRespository} _repositoy */
+    /** @property {GroupSQLiteRespository} _repositoy - SQLite repository */
     #_repository;
 
-    /** @param {GroupInMemoryRespository} repository */
+    /** @param {GroupSQLiteRespository} repository */
     constructor(repository) {
         this.#_repository = repository;
     }
 
     /** 
     * Get all groups.
-    * @returns {Map<Group>|Array} Group[] or [].
+    * @returns {Promise<Result>} Result.
     */
-    execute() {
-        return this.#_repository.getAll();
+    async execute() {
+        const groups = await this.#_repository.getAllAsync();
+        if(groups.length < 1) Result.success("Nenhum grupo foi encontrado", null);
+
+        return Result.success(`${groups.length} grupo's encontrado's`, groups);
     }
 }
