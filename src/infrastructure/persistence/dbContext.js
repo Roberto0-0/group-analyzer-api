e1ead2d589f84794676c405be407b69951f20836
Robert <roberto88526@gmail.com>
2025-08-30 00:04:20 -0300

    feat(drizzleORM): Schema added.
    It is essential for the use of query.

--- src/infrastructure/persistence/dbContext.js
+++ src/infrastructure/persistence/dbContext.js
@@ -1,5 +1,6 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
+import * as schema from "./schema/index.js";
 import { join } from "node:path";
 (await import("dotenv")).config({
     path: join(process.cwd(), ".env"),
@@ -7,4 +8,4 @@ import { join } from "node:path";
 });
 
 const sqlite = new Database(join(process.cwd(), process.env.DATA_SOURCE_DEV));
-export const db = drizzle(sqlite);
+export const db = drizzle(sqlite, { schema });
