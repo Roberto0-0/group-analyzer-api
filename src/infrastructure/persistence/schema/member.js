03736689af40868b3f5174c5857d0cec10c68093
Robert <roberto88526@gmail.com>
2025-09-01 21:30:39 -0300

    feat(member): Member Schema.

---
+++ src/infrastructure/persistence/schema/member.js
@@ -0,0 +1,18 @@
+import { sqliteTable } from "drizzle-orm/sqlite-core";
+import * as types from "drizzle-orm/sqlite-core";
+import { relations } from "drizzle-orm";
+import { membersToGroups } from "./membersToGroups.js";
+
+export const members = sqliteTable(
+    "members",
+    {
+        id: types.text("id", { length: 255 }).notNull().primaryKey(),
+        name: types.text("name", { length: 255 }).notNull(),
+        shortName: types.text("short_name", { length: 255 }).notNull() 
+    }
+);
+
+export const membersRelations = relations(members, ({ many }) => ({
+    membersToGroups: many(membersToGroups)
+}));
+
