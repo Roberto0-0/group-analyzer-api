387b5faec8238ac682a9c59cc47171ded5772b8d
Robert <roberto88526@gmail.com>
2025-09-03 18:06:23 -0300

    feat(group): BlockedModules Schema implemented.

---
+++ src/infrastructure/persistence/schema/blockedModules.js
@@ -0,0 +1,23 @@
+import { sqliteTable, index } from "drizzle-orm/sqlite-core";
+import * as type from "drizzle-orm/sqlite-core";
+import { relations } from "drizzle-orm";
+import { groups } from "./group.js";
+
+export const blockedModules = sqliteTable(
+    "blocked_modules",
+    {
+        groupId: type.text("group_id", { length: 255 })
+            .references(() => groups.id, { onDelete: "cascade" })
+            .notNull(),
+        moduleName: type.text("module_name", { length: 255 }).unique().notNull(),
+        createdAt: type.integer("created_at", { mode: "number" }).notNull()
+    },
+    (table) => [index("group_blocked_module_idx").on(table.groupId)]
+);
+
+export const blockedModulesRelations = relations(blockedModules, ({ one }) => ({
+    group: one(groups, {
+        fields: [blockedModules.groupId],
+        references: [groups.id]
+    })
+}));
