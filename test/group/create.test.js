f72ecc3627feecb5abcfe1afafcb42ed318fb4b1
Robert <roberto88526@gmail.com>
2025-09-05 11:25:15 -0300

    test(group) Request removed.

--- test/group/create.test.js
+++ test/group/create.test.js
@@ -2,7 +2,6 @@ import test from "node:test";
 import assert from "node:assert/strict";
 import { GroupCreateUsecase } from "../../src/application/usecases/group/groupCreateUsecase.js";
 import { GroupSQLiteRespository } from "../../src/infrastructure/repositories/groupSQLiteRepository.js";
-import { GroupCreateRequest } from "../../src/application/requests/groupCreateRequest.js";
 
 test("should create group.", async () => {
     const groupForm = {
@@ -13,17 +12,9 @@ test("should create group.", async () => {
         createdAt: Date.now()
     };
 
-    const request = new GroupCreateRequest(
-        groupForm.id,
-        groupForm.subject,
-        groupForm.ownerId,
-        groupForm.memberCount,
-        groupForm.createdAt,
-    );
-
     const groupRepository = new GroupSQLiteRespository();
     const createGroupUsecase = new GroupCreateUsecase(groupRepository);
-    const response = await createGroupUsecase.execute(request);
+    const response = await createGroupUsecase.execute(groupForm);
 
     if(!response.success) assert.fail(response.message);
 
