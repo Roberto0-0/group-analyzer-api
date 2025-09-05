0020947949d0ca0f0f2c5c223a8d755d7127018a
Robert <roberto88526@gmail.com>
2025-09-05 11:48:52 -0300

    docs(drizzleORM): External .env support.

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,17 +1,13 @@
 import { defineConfig } from "drizzle-kit";
-import { join } from "node:path";
 import * as dotenv from "dotenv";
 
-dotenv.config({
-    path: join(process.cwd(), ".env"),
-    quiet: true
-});
+dotenv.config();
 
 export default defineConfig({
     dialect: "sqlite",
     schema: "./src/infrastructure/persistence/schema/*.js",
     out: "./src/infrastructure/persistence/drizzle",
     dbCredentials: {
-        url: process.env.DATA_SOURCE_DEV
+        url: process.env.DATA_SOURCE
     }
 });
