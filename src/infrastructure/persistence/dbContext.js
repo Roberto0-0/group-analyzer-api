7182dc2579026a2dd9c6402e47ea76b39f7b10f2
Robert <roberto88526@gmail.com>
2025-09-05 18:52:26 -0300

    refactor(drizzleORM): External .env support.

--- src/infrastructure/persistence/dbContext.js
+++ src/infrastructure/persistence/dbContext.js
@@ -1,7 +1,11 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 import * as schema from "./schema/index.js";
-(await import("dotenv")).config();
+import { join } from "node:path";
+(await import("dotenv")).config({
+    path: join(process.cwd(), ".env"),
+    quiet: true
+});
 
-const sqlite = new Database(process.env.DATA_SOURCE);
+const sqlite = new Database(join(process.cwd(), process.env.DATA_SOURCE));
 export const db = drizzle(sqlite, { schema });
