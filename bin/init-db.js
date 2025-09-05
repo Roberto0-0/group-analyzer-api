86af39c1a62cc32b6390a03ae9a6aa3a114bb82e
Robert <roberto88526@gmail.com>
2025-09-05 18:48:43 -0300

    docs(bin): init-db implemented.
    Settings required for creating and configuring the database.

---
+++ bin/init-db.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import { execSync } from "node:child_process";
+import { existsSync } from "node:fs";
+import { join } from "node:path";
+import * as dotenv from "dotenv";
+
+dotenv.config({
+    path: join(process.cwd(), ".env"),
+    quiet: true
+});
+
+if (existsSync(join(process.cwd(), process.env.DATA_SOURCE))) process.exit(0);
+
+try {
+    execSync("npx drizzle-kit generate --name=init --config=drizzle.config.js", { stdio: "inherit" });
+    execSync("npx drizzle-kit push --config=drizzle.config.js", { stdio: "inherit" });
+} catch (error) { process.exit(1); }
