b652e7994728af9338012f982d2e10c86109afa3
Robert <roberto88526@gmail.com>
2025-09-05 11:43:33 -0300

    test(member): Group SQLite Reposity removed.

--- test/member/getByGroupId.test.js
+++ test/member/getByGroupId.test.js
@@ -1,17 +1,15 @@
 import test from "node:test";
 import assert from "node:assert/strict";
 import { MemberSQLiteRespository } from "../../src/infrastructure/repositories/memberSQLiteRepository.js";
-import { GroupSQLiteRespository } from "../../src/infrastructure/repositories/groupSQLiteRepository.js";
 import { MemberGetByGroupIdUsecase } from "../../src/application/usecases/member/memberGetByGroupIdUsecase.js";
 
 test("should get member to group.", async () => {
     const memberRepository = new MemberSQLiteRespository();
-    const groupRepository = new GroupSQLiteRespository();
 
     const id = "29302030@c.us";
     const groupId = "1293020340"; 
 
-    const getMemberByIdUsecase = new MemberGetByGroupIdUsecase(memberRepository, groupRepository);
+    const getMemberByIdUsecase = new MemberGetByGroupIdUsecase(memberRepository);
     const response = await getMemberByIdUsecase.execute(id, groupId);
 
     if (!response.success) assert.fail(response.message);
