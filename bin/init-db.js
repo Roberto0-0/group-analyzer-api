e6a4bf86c1869a115b5a4963100aa6f44686b7e5
Robert <roberto88526@gmail.com>
2025-09-05 19:43:42 -0300

    docs(bin): Local drizzle config support.

--- bin/init-db.js
+++ bin/init-db.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 import { execSync } from "node:child_process";
 import { existsSync } from "node:fs";
-import { join } from "node:path";
+import { join, resolve } from "node:path";
 import * as dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 dotenv.config({
     path: join(process.cwd(), ".env"),
@@ -11,7 +15,9 @@ dotenv.config({
 
 if (existsSync(join(process.cwd(), process.env.DATA_SOURCE))) process.exit(0);
 
+const drizzleConfigPath = resolve(__dirname, "../drizzle.config.js"); 
+
 try {
-    execSync("npx drizzle-kit generate --name=init", { stdio: "inherit" });
-    execSync("npx drizzle-kit push", { stdio: "inherit" });
+    execSync(`npx drizzle-kit generate --name=init --config=${drizzleConfigPath}`, { stdio: "inherit" });
+    execSync(`npx drizzle-kit push --config=${drizzleConfigPath}`, { stdio: "inherit" });
 } catch (error) { process.exit(1); }
