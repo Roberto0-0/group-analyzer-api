fd058f7535b599dbfbc582360a33c9de43657db6
Robert <roberto88526@gmail.com>
2025-09-05 17:26:46 -0300

    refactor(drizzleORM): DATA_SOURCE_DEV -> DATA_SOURCE.

--- src/infrastructure/persistence/dbContext.js
+++ src/infrastructure/persistence/dbContext.js
@@ -1,8 +1,7 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 import * as schema from "./schema/index.js";
-import { join } from "node:path";
 (await import("dotenv")).config();
 
-const sqlite = new Database(join(process.cwd(), process.env.DATA_SOURCE_DEV));
+const sqlite = new Database(process.env.DATA_SOURCE);
 export const db = drizzle(sqlite, { schema });
