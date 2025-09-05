954fe865c30978daefd960c692cefcf871d9b990
Robert <roberto88526@gmail.com>
2025-09-05 11:24:18 -0300

    test(group): GroupGetByIdUsecase support.

--- test/group/memberCountUpdate.test.js
+++ test/group/memberCountUpdate.test.js
@@ -2,13 +2,13 @@ import test from "node:test";
 import assert from "node:assert/strict";
 import { GroupSQLiteRespository } from "../../src/infrastructure/repositories/groupSQLiteRepository.js";
 import { GroupMemberCountUpdateUsecase } from "../../src/application/usecases/group/groupMemberCountUpdateUsecase.js";
-import { GroupGetById } from "../../src/application/usecases/group/groupGetByIdUsecase.js";
+import { GroupGetByIdUsecase } from "../../src/application/usecases/group/groupGetByIdUsecase.js";
 
 test("should get group by id.", async () => {
     const groupRepository = new GroupSQLiteRespository();
     const id = "2039403040";
 
-    const getGroupByIdUsecase = new GroupGetById(groupRepository);
+    const getGroupByIdUsecase = new GroupGetByIdUsecase(groupRepository);
     const getByIdresponse = await getGroupByIdUsecase.execute(id);
 
     const newMemberCount = getByIdresponse.data.memberCount - 70;
