c79b3b9d19796230ee24ecd848583a6cda8bf706
Robert <roberto88526@gmail.com>
2025-09-05 20:36:04 -0300

    docs(bin): __dirname -> dirname__

--- bin/init-db.js
+++ bin/init-db.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 import { execSync } from "node:child_process";
 import { existsSync } from "node:fs";
-import { join, resolve } from "node:path";
+import { join, resolve, dirname } from "node:path";
 import * as dotenv from "dotenv";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const dirname__ = dirname(fileURLToPath(import.meta.url));
 
 dotenv.config({
     path: join(process.cwd(), ".env"),
@@ -15,7 +14,7 @@ dotenv.config({
 
 if (existsSync(join(process.cwd(), process.env.DATA_SOURCE))) process.exit(0);
 
-const drizzleConfigPath = resolve(__dirname, "../drizzle.config.js"); 
+const drizzleConfigPath = resolve(dirname__, "../drizzle.config.js"); 
 
 try {
     execSync(`npx drizzle-kit generate --name=init --config=${drizzleConfigPath}`, { stdio: "inherit" });
