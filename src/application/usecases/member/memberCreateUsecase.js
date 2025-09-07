acbc12999d3e8cfca5b54407c191e94ca839b559
Robert <roberto88526@gmail.com>
2025-09-07 08:05:21 -0300

    feat(member): PostgreSQL support added.

--- src/application/usecases/member/memberCreateUsecase.js
+++ src/application/usecases/member/memberCreateUsecase.js
@@ -1,21 +1,16 @@
 import { Member } from "../../../domain/entities/Member.js";
 import { MemberToGroup } from "../../../domain/valueObject/MemberToGroup.js";
-import { membersToGroups } from "../../../infrastructure/persistence/schema/membersToGroups.js";
-import { MemberSQLiteRespository } from "../../../infrastructure/repositories/memberSQLiteRepository.js";
 import { Result } from "../../common/result.js";
+import { IMemberRepository } from "../../interfaces/IMemberRepository.js";
 
 export class MemberCreateUsecase {
-    /**
-     * @property {MemberQLiteRespository} _repository - SQLite repository.
-     */
-    #_repositoy;
+    /** @property {IMemberRepository} _repository */
+    #_repository;
 
-    /**
-     * @param {MemberSQLiteRespository} repository - SQLite repository.
-     */
+    /** @param {IMemberRepository} repository */
     constructor(repository) {
-        /** @type {MemberSQLiteRespository}*/
-        this.#_repositoy = repository;
+        /** @type {IMemberRepository}*/
+        this.#_repository = repository;
     }
 
     /**
@@ -25,14 +20,14 @@ export class MemberCreateUsecase {
      * @returns {Promise<Result>} Result.
     */
     async execute(groupId, request) {
-        const member = await this.#_repositoy.getByIdAsync(request.id);
+        const member = await this.#_repository.getByIdAsync(request.id);
         if (member) {
-            const memberAssociation = await this.#_repositoy.getByGroupId(groupId, request.id);
+            const memberAssociation = await this.#_repository.getByGroupId(groupId, request.id);
             if (!memberAssociation) {
                 const newMemberToGroup = new MemberToGroup(request.id, groupId);
                 newMemberToGroup.setXpRequired(newMemberToGroup.level);
 
-                await this.#_repositoy.addMemberToGroupAsync(newMemberToGroup);
+                await this.#_repository.addMemberToGroupAsync(newMemberToGroup);
 
                 return Result.success("Membro adiconado ao grupo com successo.", null);
             }
@@ -45,8 +40,8 @@ export class MemberCreateUsecase {
         const newMemberToGroup = new MemberToGroup(newMember.id, groupId);
         newMemberToGroup.setXpRequired(newMemberToGroup.level);
 
-        await this.#_repositoy.addAsync(newMember);
-        await this.#_repositoy.addMemberToGroupAsync(newMemberToGroup);
+        await this.#_repository.addAsync(newMember);
+        await this.#_repository.addMemberToGroupAsync(newMemberToGroup);
 
         return Result.success("Membro criado com successo.", newMember);
     }
