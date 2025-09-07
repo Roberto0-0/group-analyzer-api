f06ff92bab9de742bb47910db228b8da9c8bfb63
Robert <roberto88526@gmail.com>
2025-09-07 08:07:50 -0300

    feat(member): PostgreSQL support added.

--- src/application/usecases/member/memberJoinToGroupUsecase.js
+++ src/application/usecases/member/memberJoinToGroupUsecase.js
@@ -1,25 +1,15 @@
 import { MemberToGroup } from "../../../domain/valueObject/MemberToGroup.js";
-import { GroupSQLiteRespository } from "../../../infrastructure/repositories/groupSQLiteRepository.js";
-import { MemberSQLiteRespository } from "../../../infrastructure/repositories/memberSQLiteRepository.js";
 import { Result } from "../../common/result.js";
+import { IMemberRepository } from "../../interfaces/IMemberRepository.js";
 
 export class MemberJoinToGroupUsecase {
-    /**
-     * @property {MemberSQLiteRespository} _memberRepository - member SQLite repository.
-     * @property {GroupSQLiteRespository} _groupRepository - group SQLite repository.
-     */
-    #_memberRepositoy;
-    #_groupRepository;
+    /** @property {IMemberRepository} _repository */
+    #_repository;
 
-    /**
-     * @param {MemberSQLiteRespository} memberRepository - member SQLite repository.
-     * @param {GroupSQLiteRespository} groupRepository - SQLite repository.
-     */
-    constructor(memberRepository, groupRepository) {
-        /** @type {MemberSQLiteRespository}*/
-        this.#_memberRepositoy = memberRepository;
-        /** @type {GroupSQLiteRespository}*/
-        this.#_groupRepository = groupRepository;
+    /** @param {IMemberRepository} repository */
+    constructor(repository) {
+        /** @type {IMemberRepository}*/
+        this.#_repository = repository;
     }
 
     /**
@@ -29,15 +19,9 @@ export class MemberJoinToGroupUsecase {
      * @returns {Promise<Result>} Result.
     */
     async execute(memberId, groupId) {
-        const group = await this.#_groupRepository.getByIdAsync(groupId);
-        if (!group) return Result.failure("Grupo não encontrado.", null);
-
-        const memberToGroup = await this.#_memberRepositoy.getByGroupId(memberId, groupId);
-        if (memberToGroup) return Result.failure("O membro já está neste grupo.", null);
-
         const newMemberToGroup = new MemberToGroup(memberId, groupId);
 
-        await this.#_memberRepositoy.addMemberToGroupAsync(newMemberToGroup);
+        await this.#_repository.addMemberToGroupAsync(newMemberToGroup);
 
         return Result.success("Membro adicionado ao grupo.", null);
     }
