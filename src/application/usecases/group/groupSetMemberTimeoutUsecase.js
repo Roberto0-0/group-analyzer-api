c3dfc038fc31befd2fb4d1cfd62400dd7d1dc0c2
Robert <roberto88526@gmail.com>
2025-09-07 07:45:01 -0300

    feat(group): PostgreSQL support added.

--- src/application/usecases/group/groupSetMemberTimeoutUsecase.js
+++ src/application/usecases/group/groupSetMemberTimeoutUsecase.js
@@ -1,25 +1,15 @@
 import { MemberTimeout } from "../../../domain/valueObject/MemberTimeout.js";
-import { GroupSQLiteRespository } from "../../../infrastructure/repositories/groupSQLiteRepository.js";
-import { MemberSQLiteRespository } from "../../../infrastructure/repositories/memberSQLiteRepository.js";
 import { Result } from "../../common/result.js";
+import { IGroupRepository } from "../../interfaces/IGroupRepository.js";
 
 export class GroupSetMemberTimeoutUsecase {
-    /**
-     * @property {GroupSQLiteRespository} _groupRepository - group SQLite repository.
-     * @property {MemberSQLiteRespository} _memberRepository - member SQLite repository.
-     */
-    #_groupRepository;
-    #_memberRepositoy;
+    /** @property {IGroupRepository} _repository */
+    #_repository;
 
-    /**
-     * @param {GroupSQLiteRespository} groupRepository - SQLite repository.
-     * @param {MemberSQLiteRespository} memberRepository - member SQLite repository.
-     */
-    constructor(groupRepository, memberRepository) {
-        /** @type {GroupSQLiteRespository}*/
-        this.#_groupRepository = groupRepository;
-        /** @type {MemberSQLiteRespository}*/
-        this.#_memberRepositoy = memberRepository;
+    /** @param {IGroupRepository} repository */
+    constructor(repository) {
+        /** @type {IGroupRepository}*/
+        this.#_repository = repository;
     }
 
     /**
@@ -30,16 +20,6 @@ export class GroupSetMemberTimeoutUsecase {
      * @returns {Promise<Result>} Result
     */
     async execute(groupId, memberId, request) {
-        const [group, member, timeout] = await Promise.all([
-            this.#_groupRepository.getByIdAsync(groupId),
-            this.#_memberRepositoy.getByIdAsync(memberId),
-            this.#_groupRepository.memberTimeoutVerify(groupId, memberId),
-        ]);
-
-        if (!group) return Result.failure("Grupo não encontrado.", null);
-        if (!member) return Result.failure("Membro não encontrado.", null);
-        if (timeout) return Result.failure("Este membro já está em um timeout.", null);
-
         const newSetTimeout = new MemberTimeout(
             groupId,
             memberId,
@@ -47,7 +27,7 @@ export class GroupSetMemberTimeoutUsecase {
             request.reason,
         );
 
-        await this.#_groupRepository.addMemberTimeoutAsync(newSetTimeout);
+        await this.#_repository.addMemberTimeoutAsync(newSetTimeout);
 
         return Result.success("Timeout definido com sucesso.", newSetTimeout);
     }
