0c643907bb4f7cbdbfeb05742d0172b6e039103f
Robert <roberto88526@gmail.com>
2025-09-07 07:25:39 -0300

    feat(group): PostgreSQL support implemented.

--- src/infrastructure/persistence/schema/group.js
+++ src/infrastructure/persistence/schema/group.js
@@ -1,18 +1,18 @@
-import { sqliteTable } from "drizzle-orm/sqlite-core";
-import * as types from "drizzle-orm/sqlite-core";
+import { pgTable } from "drizzle-orm/pg-core";
+import * as types from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 import { membersToGroups } from "./membersToGroups.js";
 import { membersTimeouts } from "./membersTimeouts.js";
 import { blockedModules } from "./blockedModules.js";
 
-export const groups = sqliteTable(
+export const groups = pgTable(
     "groups",
     {
-        id: types.text("id", { length: 255 }).unique().notNull().primaryKey(),
-        name: types.text("name", { length: 255 }).notNull(),
+        id: types.varchar("id").unique().notNull().primaryKey(),
+        name: types.varchar("name").notNull(),
         memberCount: types.integer("member_count").notNull(),
-        createdAt: types.integer("created_at", { mode: "number" }).notNull(),
-        registeredAt: types.integer("registered_at", { mode: "number" }).notNull()
+        createdAt: types.bigint("created_at", { mode: "number" }).notNull(),
+        registeredAt: types.bigint("registered_at", { mode: "number" }).notNull()
     }
 );
 
