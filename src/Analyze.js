458709776f20fb0751cd94d8bea85f167a56df19
Robert <roberto88526@gmail.com>
2025-09-07 09:02:50 -0300

    refactor(analyze): Optimization.
    The group id is cached, the member is obtained directly by the association.

--- src/Analyze.js
+++ src/Analyze.js
@@ -1,5 +1,8 @@
 import { GroupController } from "./adapters/controllers/groupController.js";
 import { MemberController } from "./adapters/controllers/memberController.js";
+import NodeCache from "node-cache";
+
+const groupsCache = new NodeCache({ stdTTL: 3600, checkperiod: 7200 });
 
 export class Analyze {
     /**
@@ -30,21 +33,27 @@ export class Analyze {
         const memberId = chat.lastMessage.author;
 
         /**
-         * XP calculator
-         * @param {number} msgLength
-         */
+        * XP calculator
+        * @param {number} msgLength
+        */
         let xpCalc = (msgLength) => (msgLength > 50) ? 7 : 3;
 
-        const getGroupResponse = await this.#group.getById(groupId);
+        if (groupsCache.has(groupId)) {
+            group = groupsCache.get(groupId);
+        } else {
+            const getGroupResponse = await this.#group.getById(groupId);
 
-        if (!getGroupResponse.success) {
-            group = (await this.#group.create({
-                id: chat.id._serialized,
-                name: chat.name,
-                memberCount: chat.groupMetadata.size,
-                createdAt: chat.groupMetadata.creation * 1000
-            })).data;
-        } else { group = getGroupResponse.data; }
+            if (!getGroupResponse.success) {
+                group = (await this.#group.create({
+                    id: chat.id._serialized,
+                    name: chat.name,
+                    memberCount: chat.groupMetadata.size,
+                    createdAt: chat.groupMetadata.creation * 1000
+                })).data;
+            } else { group = getGroupResponse.data; }
+
+            groupsCache.set(group.id, group);
+        }
 
         if (group.memberCount !== chat.groupMetadata.size) {
             if (chat.groupMetadata.size < group.memberCount) {
@@ -67,49 +76,53 @@ export class Analyze {
 
         if (group.name !== chat.name) await this.#group.newName(group.id, chat.name);
 
-        const getMemberResponse = await this.#member.getByGroupId(memberId, group.id);
+        let getMemberResponse;
+
+        getMemberResponse = await this.#member.getByAssociation(memberId, group.id);
 
         if (!getMemberResponse.success) {
-            if (getMemberResponse.data) {
-                await this.#member.addMemberToGroup(memberId, group.id);
+            getMemberResponse = await this.#member.getByGroupId(memberId, group.id);
+
+            if (!getMemberResponse.success) {
+                if (getMemberResponse.data) {
+                    await this.#member.addMemberToGroup(memberId, group.id);
+
+                    await this.#member.statusUpdate(
+                        memberId,
+                        group.id,
+                        { xp: xpCalc(chat.lastMessage.body.length) }
+                    );
+                    return;
+                }
+
+                member = (await this.#member.create(
+                    group.id,
+                    {
+                        id: memberId,
+                        name: chat.lastMessage._data.notifyName
+                    }
+                )).data;
 
                 await this.#member.statusUpdate(
                     memberId,
                     group.id,
                     { xp: xpCalc(chat.lastMessage.body.length) }
                 );
+
                 return;
             }
-
-            member = (await this.#member.create(
-                group.id,
-                {
-                    id: memberId,
-                    name: chat.lastMessage._data.notifyName
-                }
-            )).data;
-
-            await this.#member.statusUpdate(
-                memberId,
-                group.id,
-                { xp: xpCalc(chat.lastMessage.body.length) }
-            );
-
-            return;
         }
 
         member = getMemberResponse.data;
-
-        if (member.name !== chat.lastMessage._data.notifyName) await this.#member.newName(member.id, chat.lastMessage._data.notifyName)
 
         const newXp = member.xp + xpCalc(chat.lastMessage.body.length);
 
         if (newXp >= member.xpRequired) {
             member.xp = newXp;
-            const { level, xp, xpRequired } = this.#member.xpManager(group.id, member).data;
+            const { level, xp, xpRequired } = this.#member.xpManager(memberId, group.id, member).data;
 
             await this.#member.statusUpdate(
-                member.id,
+                memberId,
                 group.id,
                 {
                     level,
@@ -124,7 +137,7 @@ export class Analyze {
         }
 
         await this.#member.statusUpdate(
-            member.id,
+            memberId,
             group.id,
             {
                 xp: newXp,
