7a4e950fa65e59cf0e2865b195e036e176da5298
Robert <roberto88526@gmail.com>
2025-09-07 07:50:10 -0300

    test(group): PostgreSQL support added.

--- test/group/blockedModuleVerify.test.js
+++ test/group/blockedModuleVerify.test.js
@@ -1,17 +1,22 @@
-import test from "node:test";
+import test, { after } from "node:test";
 import assert from "node:assert/strict";
-import { GroupSQLiteRespository } from "../../src/infrastructure/repositories/groupSQLiteRepository.js";
 import { GroupVerifyBlockedModuleUsecase } from "../../src/application/usecases/group/groupVerifyBlockedModuleUsecase.js";
+import { GroupPgRepository } from "../../src/infrastructure/repositories/groupPgRepository.js";
+import { db } from "../../src/infrastructure/persistence/dbContext.js";
 
 test("should verify block module.", async () => {
     const id = "1293020340";
     const moduleName = "Fun";
 
-    const repository = new GroupSQLiteRespository();
+    const repository = new GroupPgRepository();
     const verifyBlockedModuleUsecase = new GroupVerifyBlockedModuleUsecase(repository);
     const response = await verifyBlockedModuleUsecase.execute(id, moduleName);
 
     if (!response.success) assert.fail(response.message);
 
     assert.equal(response.success, true);
+
+    after(async () => {
+        await db.$client.end();
+    });
 });
