cd526ada18df5ad0e45cdfd52b181429f0d661c6
Robert <roberto88526@gmail.com>
2025-09-07 07:16:22 -0300

    docs(drizzleORM): PostgreSQL support implemented.

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,20 +1,17 @@
 import { defineConfig } from "drizzle-kit";
-import { join, resolve, dirname } from "node:path";
+import { resolve, dirname } from "node:path";
 import * as dotenv from "dotenv";
 import { fileURLToPath } from "url";
 
 const dirname__ = dirname(fileURLToPath(import.meta.url));
 
-dotenv.config({
-    path: join(process.cwd(), ".env"),
-    quiet: true
-});
+dotenv.config({ quiet: true });
 
 export default defineConfig({
-    dialect: "sqlite",
+    dialect: "postgresql",
     schema: resolve(dirname__, "./src/infrastructure/persistence/schema/*.js"),
     out: resolve(dirname__, "./src/infrastructure/persistence/migrations"),
     dbCredentials: {
-        url: join(process.cwd(), process.env.DATA_SOURCE)
+        url: process.env.DATABASE_URL 
     }
 });
