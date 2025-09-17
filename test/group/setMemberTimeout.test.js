6fae2e65e9997a3ea23bf730ba616a26d85c9df7
Robert <gh.robert99@gmail.com>
2025-09-17 10:53:54 -0300

    refactor(test): PostgreSQL support added.

--- test/group/setMemberTimeout.test.js
+++ test/group/setMemberTimeout.test.js
@@ -1,24 +1,27 @@
-import test from "node:test";
+import test, { after } from "node:test";
 import assert from "node:assert/strict";
 import { GroupSetMemberTimeoutUsecase } from "../../src/application/usecases/group/groupSetMemberTimeoutUsecase.js";
-import { MemberSQLiteRespository } from "../../src/infrastructure/repositories/memberSQLiteRepository.js";
-import { GroupSQLiteRespository } from "../../src/infrastructure/repositories/groupSQLiteRepository.js";
+import { GroupPgRepository } from "../../src/infrastructure/repositories/groupPgRepository.js";
+import { db } from "../../src/infrastructure/persistence/dbContext.js";
 
 test("should set member timeout.", async () => {
-    const memberRepository = new MemberSQLiteRespository();
-    const groupRepository = new GroupSQLiteRespository();
+    const repository = new GroupPgRepository();
 
-    const groupId = "1293020340";
-    const memberId = "29392030@c.us";
+    const groupId = "2930203002029@c.us";
+    const memberId = "29393020300@c.us";
     const request = {
         timeRef: "30m",
-        reason: "Mucho gay"
+        reason: "Message spam"
     }
 
-    const setMemberTimeouot = new GroupSetMemberTimeoutUsecase(groupRepository, memberRepository);
+    const setMemberTimeouot = new GroupSetMemberTimeoutUsecase(repository);
     const response = await setMemberTimeouot.execute(groupId, memberId, request);
 
     if (!response.success) assert.fail(response.message);
 
     assert.equal(response.success, true);
+
+    after(async () => {
+        await db.$client.end();
+    });
 });
